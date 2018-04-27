33458c3815de313f02209d0baf4f9d36fb773b70
cwatson45 <cwatson45@users.noreply.github.com>
2018-04-27 09:26:23 -0400

    architecture changed

--- cnn.py
+++ cnn.py
@@ -17,7 +17,10 @@ from keras.models import Sequential
 from gensim.models import Word2Vec
 from keras.preprocessing import sequence
 from keras.models import Model
-from keras.layers import Dense, Dropout, Embedding, LSTM, Input, merge, InputSpec, TimeDistributed, BatchNormalization, Bidirectional, Wrapper, Concatenate, concatenate
+from keras.layers import Dense, Dropout, Embedding, LSTM, Input, merge, InputSpec, 
+        TimeDistributed, BatchNormalization, Bidirectional, Wrapper, Concatenate, concatenate,
+        Conv2D, MaxPooling2D, Flatten
+
 from keras.optimizers import RMSprop, Adam
 from keras.utils import np_utils
 from keras import regularizers
