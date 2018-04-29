0f930d8dbba6be30e34c6d79d8694c5618a4ebc9
cwatson45 <cwatson45@users.noreply.github.com>
2018-04-29 10:43:32 -0400

    update

--- cnn1d.py
+++ cnn1d.py
@@ -309,8 +309,8 @@ output = Dense(len(unique_train_label), activation='softmax')(after_dp)
 model = Model(inputs=sequence_embed, outputs=output)
 '''
 
-X_train = np.expand_dims(X_train, axis = 3)
-X_test = np.expand_dims(X_test, axis = 3)
+#X_train = np.expand_dims(X_train, axis = 3)
+#X_test = np.expand_dims(X_test, axis = 3)
 
 input_shape = (max_sentence_len, embed_size_word2vec,1) #max sentence length
 model = Sequential()
@@ -318,13 +318,13 @@ model.add(Conv2D(32, kernel_size=(5, embed_size_word2vec), strides=1,
                  activation='relu',
                  input_shape=input_shape))
 model.add(Dropout(rate = .5))
-model.add(MaxPooling1D(pool_size=2, strides=2))
+#model.add(MaxPooling1D(pool_size=2, strides=2))
 model.add(Conv1D(64, 5, activation='relu'))
 model.add(Dropout(rate = .5))
-model.add(MaxPooling1D(pool_size=2))
+#model.add(MaxPooling1D(pool_size=2))
 model.add(Conv1D(64, 5, activation='relu'))
 model.add(Dropout(rate = .5))
-model.add(MaxPooling1D(pool_size=2))
+#model.add(MaxPooling1D(pool_size=2))
 model.add(Flatten())
 model.add(Dense(1000, activation='relu'))
 model.add(Dense(len(unique_train_label), activation='softmax'))
